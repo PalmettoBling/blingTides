793929c08cc1bf76ce88df0f868d0070e8e7f666
Merrick <merrickm@live.com>
2025-07-30 18:53:23 -0400

    API and chart init

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+import Chart from 'chart.js/auto';
+
+const baseUri = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter`;
+
+function tidesCall() {
+    const apiDetails = `station=8665530&date=latest&product=predictions&datum=STND&time_zone=lst_ldt&units=english&format=json`;
+    const url = `${baseUri}?${apiDetails}`;
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        });
+}
+
+function graphTides() {
+    tidesCall()
+        .then(data => {
+            let predictionData = data.predictions;
+            const ctx = document.getElementById('tidesGraph');
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    datasets: [{
+                        data: predictionData
+                    }]
+                },
+                options: {
+                    parsing: {
+                        xAxisKey: 't',
+                        yAxisKey: 'v'
+                    }
+                }
+            })
+
+        })
+        .catch(error => console.error('Error fetching tide data:', error));
+}
