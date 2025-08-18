9bd3c5b4e23194eeed737574c3d3dbb720d7306a
Merrick <merrickm@live.com>
2025-08-17 20:58:10 -0400

    functional pages breakout init.

--- assets/js/graphTides.js
+++ assets/js/graphTides.js
@@ -1,34 +1,19 @@
-function graphTides() {
-    const apiUri = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?station=8665530&date=latest&product=predictions&datum=STND&time_zone=lst_ldt&units=english&format=json`;
-    
-    fetch(apiUri)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+export function graphTides(predictionData) {
+    const tidesGraph = document.getElementById('tidesGraph');
+    new Chart(tidesGraph, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Tide Predictions',
+                tension: 1,
+                data: predictionData
+            }]
+        },
+        options: {
+            parsing: {
+                xAxisKey: 't',
+                yAxisKey: 'v'
             }
-            let data = response.json();
-            let responseData = data.predictions;
-            
-            const ctx = document.getElementById('tidesGraph');
-
-            new Chart(ctx, {
-                type: 'line',
-                data: {
-                    datasets: [{
-                        data: predictionData
-                    }]
-                },
-                options: {
-                    parsing: {
-                        xAxisKey: 't',
-                        yAxisKey: 'v'
-                    }
-                }
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}
-
-export { graphTides };
+        }
+    });
+};
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
-import { graphTides } from './graphTides.js';
+import { graphTides } from "./graphTides.js";
 
-graphTides();
+const apiUri = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=latest&station=8665530&product=predictions&datum=STND&time_zone=lst_ldt&units=english&format=json`;
+                
+fetch(apiUri)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        return response.json();
+    })
+    .then(data => {
+        let predictionData = data.predictions;
+        
+        graphTides(predictionData);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
