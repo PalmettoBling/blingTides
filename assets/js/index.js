4e715e5af51e37bf50946e81db81b91a97c409fe
Merrick <merrickm@live.com>
2025-08-20 22:24:42 -0400

    made a hot mess of default function values to set API URI, but it's late.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,12 @@
 import { graphTides } from "./graphTides.js";
 
-var apiDefaults = {};
-setApiData();
 
-fetch(craftApiUrl(apiDefaults))
+const tidesPredictionUri = new URL("https://api.tidesandcurrents.noaa.gov/api/prod/datagetter");
+let today = new Date();
+let beginDate = today.toISOString().slice(0,10).replace(/-/g,"");
+craftApiUri({"beginDate": beginDate});
+
+/*fetch(tidesPredictionUri)
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -18,21 +21,27 @@ fetch(craftApiUrl(apiDefaults))
     })
     .catch(error => {
         console.error('Error fetching data:', error);
-    });
+    });*/
 
-function craftApiUrl(apiRequestData) {
-    let baseUri = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?";
-    let datafulUri = baseUri + new URLSearchParams(apiRequestData).toString();
-    console.log("Crafted API URL:", datafulUri);
-    return datafulUri;
+function craftApiUri(apiRequestData) {
+    console.log("Base URI:", tidesPredictionUri.toString());
+    console.log("Crafting API URL with data:", apiRequestData);)
+    tidesPredictionUri.searchParams.set(apiRequestData);
+    console.log("Crafted API URL:", tidesPredictionUri.toString());
+    return;
 }
 
-function setApiData(apiDataToSet) {
-    var today = new Date();
-    var beginDate = today.toISOString().slice(0,10).replace(/-/g,"");
+// IF NOT DEFINED, SET DEFAULTS
+// IF DEFINED, SET TO THOSE VALUES
+function craftApiUri({beginDate, range="48", station="8665530", product="predictions", datum="STND", time_zone="lst_ldt", units="english", format="json"} = {}) {
 
+    console.log("Begin Date set to:", beginDate);
+    
     // Need to verify edge conditions for date changeover at UTC midnight
     // Need to verify edge conditions for API call data if not passed default, would resetting it to default cause issues?
+
+
+    /* Replacing with URL object vs string concatenation and data object
     apiDefaults.begin_date = apiDataToSet.begin_date ?? beginDate;
     apiDefaults.range = apiDataToSet.range ?? "48";
     apiDefaults.station = apiDataToSet.station ?? "8665530";
@@ -41,4 +50,5 @@ function setApiData(apiDataToSet) {
     apiDefaults.time_zone = apiDataToSet.time_zone ?? "lst_ldt";
     apiDefaults.units = apiDataToSet.units ?? "english";
     apiDefaults.format = apiDataToSet.format ?? "json";
+    */
 }
